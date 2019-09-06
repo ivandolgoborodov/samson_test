0d34eef050bd3e585d8ecc30b2a9caa346153015
ivandolgoborodov <ivandolgoborodov@yandex.ru>
2019-09-06 15:48:40 +0300

    mySortForKey

--- 2.php
+++ 2.php
@@ -8,7 +8,17 @@ function convertString($a, $b){
 	}
 	return $result;
 }
+function mySortForKey($a, $b){
+    $sortArr = array();
+    foreach($a as $key=>$val){
+		if (!$val[$b]) throw new Exception("Отсутствует ключ $b в $key - м массиве!");
+        $sortArr[$key] = $val[$b];
+    }
+    array_multisort($sortArr,$a);
+    return $a;
+}
 echo convertString('twostdddtwostbbbtwostaaa', 'twost');
-
+$a=array(['a'=>2,'b'=>1],['a'=>1,'b'=>6],['a'=>8,'b'=>2],['a'=>9,'b'=>-4],['g'=>5,'b'=>3]);
+echo '<pre>';print_r(mySortForKey($a,'a'));echo '</pre>';
 
 ?>
